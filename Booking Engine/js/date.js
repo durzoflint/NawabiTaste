351d82ffb74d0baa286e5252c805e0b9a29beead
durzoflint <abhinavu1201@gmail.com>
2017-12-14 18:34:58 +0530

    Made the table responsive

--- Booking Engine/js/date.js
+++ Booking Engine/js/date.js
@@ -4,7 +4,7 @@ for (var i = 0; i < 30; i++)
 {
 	var priorDate = new Date().setDate(today.getDate()+i);
 	var mydate = new Date(priorDate);
-	var tableData = document.getElementsByTagName("td")[i];
+	var tableData = document.getElementsByClassName("mydiv")[i];
 	document.getElementsByClassName("Lunch")[i].value = "Lunch " + mydate.getDate()+"/"+(mydate.getMonth()+1)+"/"+mydate.getFullYear();
 	document.getElementsByClassName("Dinner")[i].value = "Dinner " + mydate.getDate()+"/"+(mydate.getMonth()+1)+"/"+mydate.getFullYear();
 	tableData.innerHTML = mydate.getDate()+"/"+(mydate.getMonth()+1)+"/"+mydate.getFullYear()+"<br>"+days[mydate.getDay()] + "<br>" + tableData.innerHTML;
